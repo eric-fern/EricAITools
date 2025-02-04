2affd69407afa6fd5290d3a31e966301eedad3c3
Eric Fernandez <eric.fernandez5@outlook.com>
2025-02-04 10:21:02 -0800

    package added Aspire.hosting.azure.storage

--- tools.AppHost/Program.cs
+++ tools.AppHost/Program.cs
@@ -6,7 +6,7 @@ builder.AddProject<Projects.tools_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
     .WaitFor(apiService);
-
+//Storage required a package - packages\aspire.hosting.azure.storage\9.0.0\
 var storage = builder.AddAzureStorage("storage")
     .AddBlobs("content-store");
 
