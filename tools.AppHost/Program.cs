8d79baa17f5cb7d2dfb926f5fdd2f92d0f315ac0
Eric Fernandez <eric.fernandez5@outlook.com>
2025-02-04 21:54:12 -0800

    added gateway API controller

--- tools.AppHost/Program.cs
+++ tools.AppHost/Program.cs
@@ -1,14 +1,19 @@
+//builder will make distributed app
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.tools_ApiService>("api");
+//Storage required a package - packages\aspire.hosting.azure.storage\9.0.0\
+//add storage and a folder of blobs.
+var storage = builder.AddAzureStorage("storage")
+    .AddBlobs("content-store");
+// Your existing API service definition gets updated to include the storage reference
+var apiService = builder.AddProject<Projects.tools_ApiService>("api")
+    .WithReference(storage);  // This line connects the API to the storage
 
 builder.AddProject<Projects.tools_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
     .WaitFor(apiService);
-//Storage required a package - packages\aspire.hosting.azure.storage\9.0.0\
-var storage = builder.AddAzureStorage("storage")
-    .AddBlobs("content-store");
+
 
 
 builder.Build().Run();
